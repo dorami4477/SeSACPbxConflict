1214665bde7a63e7130b6b479bbf2bda6502822a
dahyun <dahyun4477@gmail.com>
2024-10-08 11:14:03 +0900

    Initial Commit

---
+++ SeSACPbxConflict/SeSACPbxConflictApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSACPbxConflictApp.swift
+//  SeSACPbxConflict
+//
+//  Created by 박다현 on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SeSACPbxConflictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
