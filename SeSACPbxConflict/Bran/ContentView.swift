fd99ea126236cc55607624df20335caf341498f4
dahyun <dahyun4477@gmail.com>
2024-10-08 13:33:20 +0900

    change folder

--- SeSACPbxConflict/Bran/ContentView.swift
+++ SeSACPbxConflict/Bran/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, bran2!")
+            Text("Hello, den2!")
         }
         .padding()
     }
